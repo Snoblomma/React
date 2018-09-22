6646579606e79b07f523c08bf75bcd408271f83e
Kate Kate <snoblomma@gmail.com>
2018-09-23 00:16:46 +0100

    updating rendered element

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -6,14 +6,34 @@ function formatName(user) {
     return user.firstName + ' ' + user.lastName;
 }
 
-const user = {
-    firstName: 'Kate',
-    lastName: 'Snoblomma'
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
 }
 
-const element = <h1>Hello, {formatName(user)}!</h1>;
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    // highlight-next-line
+    ReactDOM.render(element, document.getElementById('root'));
+}
 
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+setInterval(tick, 1000);
+
+// const user = {
+//     firstName: 'Kate',
+//     lastName: 'Snoblomma'
+// }
+
+// const element = getGreeting(user);
+
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// );
