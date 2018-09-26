f3f84df48f5b0d67eb0f7e7d79ec79144162571a
Kate Kate <snoblomma@gmail.com>
2018-09-26 20:05:59 +0100

    encapsulating Clock class

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,38 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
+function Clock(props) {
+    return (
+        <div>
 
-function getGreeting(user) {
-    if (user) {
-        return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
+            <h1>Hello, world!</h1>
+
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
+
+        </div>
+    );
 }
 
 function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('root')
     );
-    // highlight-next-line
-    ReactDOM.render(element, document.getElementById('root'));
 }
 
 setInterval(tick, 1000);
-
-// const user = {
-//     firstName: 'Kate',
-//     lastName: 'Snoblomma'
-// }
-
-// const element = getGreeting(user);
-
-// ReactDOM.render(
-//     element,
-//     document.getElementById('root')
-// );
