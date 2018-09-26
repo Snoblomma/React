17030d3b31e091c830d979c050352879af6973ad
Kate Kate <snoblomma@gmail.com>
2018-09-26 20:45:35 +0100

    clock tick every second thanks to lifecycle methods

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -4,19 +4,41 @@ import './index.css';
 
 //components defined as classes have some additional features (state in this case)
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillMount() {
+        clearInterval(this.timerID);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render() {
         return (
-           <div>
-               <h1>Hello, world!</h1>
-               <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
-           </div>
-       );
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
     }
 }
 
 function tick() {
     ReactDOM.render(
-        <Clock date={new Date()} />,
+        <Clock />,
         document.getElementById('root')
     );
 }
