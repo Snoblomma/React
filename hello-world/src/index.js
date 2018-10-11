39fbfc2db7f5b7d99ae7d4b910914519bd2a3436
Kate Kate <snoblomma@gmail.com>
2018-10-11 22:50:34 +0100

    textarea uses value attribute

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,78 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Post(props) {
-    return (
-        <div key={props.id}>
-            <h3>{props.title}</h3>
-            <p>{props.content}</p>
-        </div>
-    );
+class EssayForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'Please write an essay about your favorite DOM element.'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Essay:
+            <textarea value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
 }
-
-function Blog(props) {
-    const sidebar = (
-        <ul>
-            {props.posts.map((post) =>
-                <li key={post.id}>
-                    {post.title}
-                </li>
-            )}
-        </ul>
-    );
-
-    const content = posts.map((post) =>
-        <Post
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            content={post.content} />
-    );
-    return (
-        <div>
-            {sidebar}
-            <hr />
-            {content}
-        </div>
-    );
-}
-
-const posts = [
-    { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
-    { id: 2, title: 'Installation', content: 'You can install React from npm.' }
-];
 
 class NameForm extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
+
     handleChange(event) {
-      this.setState({value: event.target.value});
+        this.setState({ value: event.target.value });
     }
-  
+
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
     }
-  
+
     render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
             <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
     }
-  }
+}
 ReactDOM.render(
-    <NameForm />,
+    <EssayForm />,
     document.getElementById('root')
 );
